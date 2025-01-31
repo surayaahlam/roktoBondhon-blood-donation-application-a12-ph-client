7e156dacc28d07cb65fbcba81de814e0d92969fb
surayaahlam <surayaahlam@gmail.com>
2025-02-01 04:22:56 +0600

    blog page and blog detail page added

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,9 +1,42 @@
+import { Helmet } from "react-helmet-async";
+import Heading from "../components/shared/Heading";
+import Loading from "./Loading";
+import { useParams } from "react-router-dom";
+import useAxios from "../hooks/useAxios";
+import { useQuery } from "@tanstack/react-query";
 
 const BlogDetails = () => {
+    const { id } = useParams();
+    const axiosPublic = useAxios();
+
+    const { data: blog = [], isLoading } = useQuery({
+        queryKey: ['view-blog', id],
+        enabled: !!id,
+        queryFn: async () => {
+            const response = await axiosPublic(`/published-blog/${id}`);
+            return response.data;
+        }
+    });
+
+    if (isLoading) return <Loading />
     return (
-        <div>
-            Blog Details
-        </div>
+        <div className="container mx-auto mt-10 mb-28">
+            <Helmet>
+                <title>Rokto Bondhon | Blog Details</title>
+            </Helmet>
+            <div className="w-11/12 md:w-10/12 lg:w-8/12 mx-auto">
+                <Heading title="Blog Details" />
+                <div className="card p-8 shadow-md rounded-2xl flex flex-col gap-5">
+                    <img className="w-full h-[500px] object-cover rounded-xl" src={blog.thumbnail} alt="" />
+                    <h2 className="text-3xl md:text-4xl lg:text-3xl font-bold">{blog.title}</h2>
+                    <div
+                        className="flex flex-col text-lg text-font_secondary font-nunito font-medium"
+                        dangerouslySetInnerHTML={{ __html: blog.content }}
+                    ></div>
+
+                </div>
+            </div>
+        </div >
     );
 };
 
--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,8 +1,44 @@
+import { Helmet } from "react-helmet-async";
+import Heading from "../components/shared/Heading";
+import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import useAxios from "../hooks/useAxios";
+import Loading from "./Loading";
 
 const BlogPage = () => {
+    const axiosPublic = useAxios();
+    const navigate = useNavigate();
+
+    const { data: blogs = [], isLoading } = useQuery({
+        queryKey: ["published-blogs"],
+        queryFn: async () => {
+            const res = await axiosPublic(`/published-blogs`);
+            return res.data;
+        }
+    });
+
+    if (isLoading) return <Loading></Loading>;
+
     return (
-        <div>
-            Blog Page
+        <div className="container mx-auto mt-10 mb-28">
+            <Helmet>
+                <title>Rokto Bondhon | Blog Page</title>
+            </Helmet>
+            <div className="w-11/12 lg:w-9/12 mx-auto">
+                <Heading subtitle="Blog Page" title="Explore Our Latest Insights and Stories" />
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
+                    {blogs.map((blog) => (
+                        <div key={blog._id} className="card p-6 rounded-2xl shadow-md flex flex-col gap-3">
+                            <img className="w-full h-[230px] object-cover rounded-xl shadow" src={blog?.thumbnail} alt="" />
+                            <h2 className="text-xl text-font_primary font-bold">{blog.title}</h2>
+                            <div>
+                                <button onClick={() => navigate(`/blog/${blog._id}`)} className="btn bg-primary border-none text-white hover:bg-secondary text-sm uppercase px-10">View</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
