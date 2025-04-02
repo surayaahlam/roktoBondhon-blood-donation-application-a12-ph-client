0445c000d92300117e0b988d45d4d33845a1b737
surayaahlam <surayaahlam@gmail.com>
2025-04-02 07:00:20 +0600

    scroll to top added for links in footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import useAuth from "../hooks/useAuth";
+import ScrollToTop from "../components/shared/ScrollToTop";
 
 const MainLayout = () => {
     const { user } = useAuth();
     return (
         <div className="font-roboto">
+            <ScrollToTop />
             <Navbar></Navbar>
             <div className={`${user?.email ? "min-h-[calc(100vh-691.5px)]" : "min-h-[calc(100vh-685.9px)]"}`}>
                 <Outlet></Outlet>
